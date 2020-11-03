81672196ec4e6292041abc9e00aba15e7ec8c7ef
unknown <vahnxp@gmail.com>
2020-11-02 21:05:40 -0300

    test port fix

--- app/routers/router.go
+++ app/routers/router.go
@@ -1,7 +1,9 @@
 package routers
 
 import (
+	"fmt"
 	"os"
+	"strconv"
 
 	middlewareutils "github.com/Tsuryu/arreglapp-transactions/app/middlewares/utils"
 	"github.com/gin-gonic/gin"
@@ -9,12 +11,17 @@ import (
 
 // Router : app routes
 func Router() {
-
 	router := gin.Default()
 	router.GET("/health", middlewareutils.GetMiddlewares("health")...)
 	router.POST("/transaction/:id/detail", middlewareutils.GetMiddlewares("postTransactionDetail")...)
 	router.POST("/transaction", middlewareutils.GetMiddlewares("postTransaction")...)
 	router.GET("/transaction", middlewareutils.GetMiddlewares("getTransactions")...)
 	router.GET("/transaction/:id", middlewareutils.GetMiddlewares("getTransaction")...)
-	router.Run(":" + os.Getenv("APP_PORT"))
+
+	port, err := strconv.Atoi(os.Getenv("APP_PORT"))
+	if err != nil {
+		fmt.Println("Eror loading port from env: " + os.Getenv("APP_PORT"))
+		port = 8080
+	}
+	router.Run(":" + strconv.Itoa(port))
 }
