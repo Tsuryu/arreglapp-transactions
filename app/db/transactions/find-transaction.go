05ac114e0474ac25c0765c8de31fb147eb595189
unknown <vahnxp@gmail.com>
2020-11-28 12:26:23 -0300

    push notification

--- app/db/transactions/find-transaction.go
+++ app/db/transactions/find-transaction.go
@@ -5,21 +5,26 @@ import (
 	"time"
 
 	"github.com/Tsuryu/arreglapp-commons/app/models"
+	"github.com/Tsuryu/arreglapp-commons/app/utils"
 	"go.mongodb.org/mongo-driver/bson"
 )
 
 // FindBy : fetch a transaction by id
-func FindBy(id string) (*models.Transaction, error) {
+func FindBy(id string) (models.Transaction, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 
 	condition := bson.M{"trace_id": id}
 
-	var result *models.Transaction
+	var result models.Transaction
 
 	err := Collection.FindOne(ctx, condition).Decode(&result)
 	if err != nil {
-		return nil, err
+		return result, err
+	}
+
+	for index, detail := range result.Details {
+		result.Details[index].Metadata = utils.GetObjectFromPrimitiveD(detail.Metadata)
 	}
 
 	return result, nil
